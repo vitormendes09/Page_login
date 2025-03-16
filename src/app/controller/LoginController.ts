39e20b7cfbd2285e4114153fa31d684d302af4f2
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-03-16 12:30:02 -0300

    create controller

---
+++ src/app/controller/LoginController.ts
@@ -0,0 +1,31 @@
+import {useState} from "react"
+import {LoginData} from "../contract/loginData"
+
+export const useLoginController = () => {
+    const [formData, setFormData] = useState<LoginData>({
+        name: "",
+        age: 0,
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prev) => ({
+          ...prev,
+          [name]: name === "age" ? Number(value) : value,
+        }));
+      };
+    
+      const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Form Data:", formData);
+      };
+    
+      return {
+        formData,
+        handleChange,
+        handleSubmit,
+      };
+}
+
